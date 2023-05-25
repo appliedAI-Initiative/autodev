40373ffda4fe7813a2c53e6626f786ebca00858c
Dominik Jain <d.jain@appliedai.de>
2023-05-25 12:39:56 +0200

    Apply direct editor streaming to further actions

--- idea-plugin/autodev-plugin/src/main/java/de/appliedai/autodev/actions/AddDocstringsAction.java
+++ idea-plugin/autodev-plugin/src/main/java/de/appliedai/autodev/actions/AddDocstringsAction.java
@@ -1,11 +1,11 @@
 package de.appliedai.autodev.actions;
 
 import de.appliedai.autodev.ServiceClient;
-import de.appliedai.autodev.actions.base.GenerateToolWindowResponseEditorActionStreamed;
+import de.appliedai.autodev.actions.base.WriteToEditorActionStreamed;
 
 import java.io.IOException;
 
-public class AddDocstringsAction extends GenerateToolWindowResponseEditorActionStreamed {
+public class AddDocstringsAction extends WriteToEditorActionStreamed {
     @Override
     protected ServiceClient.StreamedResponse generateResponse(String code) throws IOException {
         return client.callCodeFunctionStreamed("add-docstrings", code);
--- idea-plugin/autodev-plugin/src/main/java/de/appliedai/autodev/actions/ImproveCodeAction.java
+++ idea-plugin/autodev-plugin/src/main/java/de/appliedai/autodev/actions/ImproveCodeAction.java
@@ -1,11 +1,11 @@
 package de.appliedai.autodev.actions;
 
 import de.appliedai.autodev.ServiceClient;
-import de.appliedai.autodev.actions.base.GenerateToolWindowResponseEditorActionStreamed;
+import de.appliedai.autodev.actions.base.WriteToEditorActionStreamed;
 
 import java.io.IOException;
 
-public class ImproveCodeAction extends GenerateToolWindowResponseEditorActionStreamed {
+public class ImproveCodeAction extends WriteToEditorActionStreamed {
     @Override
     protected ServiceClient.StreamedResponse generateResponse(String code) throws IOException {
         return client.callCodeFunctionStreamed("improve-code", code);
