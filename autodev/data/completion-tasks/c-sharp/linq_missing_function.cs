296d6fc9e8c844cafbd855f64ad6b09d699cf7d6
Clifford Wilmot <c.wilmot@appliedai.de>
2023-06-02 14:41:55 +0200

    LINQ examples

---
+++ autodev/data/completion-tasks/c-sharp/linq_missing_function.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public class EmployeeAnalysis
+{
+	public static void Main()
+	{
+        // Example data
+		IList<Employee> employees = new List<Employee>() {
+			new Employee() { EmployeeID = 15, EmployeeName = "Alice", Age = 15 } ,
+				new Employee() { EmployeeID = 12, EmployeeName = "Bob",  Age = 21 } ,
+				new Employee() { EmployeeID = 33, EmployeeName = "Charlie",  Age = 18 } ,
+				new Employee() { EmployeeID = 71, EmployeeName = "David" , Age = 20 } ,
+				new Employee() { EmployeeID = 21, EmployeeName = "Emily" , Age = 21 }
+		};
+
+		var adultEmployeeNames = GetAdultEmployeeNames(employees); // Adults are over the age of 18 (inclusive).
+		foreach(var name in adultEmployeeNames)
+		{
+			Console.WriteLine(name);
+		}
+	}
+
+	<todo>
+}
+
+public class Employee{
+	public int EmployeeID { get; set; }
+	public string EmployeeName { get; set; }
+	public int Age { get; set; }
+}
