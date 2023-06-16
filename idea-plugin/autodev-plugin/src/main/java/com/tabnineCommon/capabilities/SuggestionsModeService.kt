98c68e02ba8b0efb3dfaf560663acac9e38c8cb5
Dominik Jain <d.jain@appliedai.de>
2023-06-16 17:53:48 +0200

    Added SuggestionsModeService with hard-coded response

---
+++ idea-plugin/autodev-plugin/src/main/java/com/tabnineCommon/capabilities/SuggestionsModeService.kt
@@ -0,0 +1,8 @@
+package com.tabnineCommon.capabilities
+
+class SuggestionsModeService {
+    fun getSuggestionMode(): SuggestionsMode {
+        System.out.println("Called getSuggestionMode")
+        return SuggestionsMode.INLINE;
+    }
+}
