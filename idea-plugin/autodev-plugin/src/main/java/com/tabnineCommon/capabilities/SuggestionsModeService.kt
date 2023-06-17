99366ccf47883be0e33c7fd715af0a54164f5b60
Dominik Jain <d.jain@appliedai.de>
2023-06-17 16:31:05 +0200

    Changed mode to enable (only) inline completions

--- idea-plugin/autodev-plugin/src/main/java/com/tabnineCommon/capabilities/SuggestionsModeService.kt
+++ idea-plugin/autodev-plugin/src/main/java/com/tabnineCommon/capabilities/SuggestionsModeService.kt
@@ -3,6 +3,6 @@ package com.tabnineCommon.capabilities
 class SuggestionsModeService {
     fun getSuggestionMode(): SuggestionsMode {
         System.out.println("Called getSuggestionMode")
-        return SuggestionsMode.HYBRID;
+        return SuggestionsMode.INLINE;
     }
 }
