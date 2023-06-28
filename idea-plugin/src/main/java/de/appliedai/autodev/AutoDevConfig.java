e44542cec3af572228e93911ccf6cf93cc66baea
Dominik Jain <d.jain@appliedai.de>
2023-06-28 12:22:26 +0200

    Enabled auto-request

--- idea-plugin/src/main/java/de/appliedai/autodev/AutoDevConfig.java
+++ idea-plugin/src/main/java/de/appliedai/autodev/AutoDevConfig.java
@@ -4,6 +4,6 @@ import com.tabnineCommon.capabilities.SuggestionsMode;
 
 public class AutoDevConfig {
     public static boolean useDummyCompletions = false;
-    public static boolean autoRequestCompletionsOnDocumentChange = false;
+    public static boolean autoRequestCompletionsOnDocumentChange = true;
     public static SuggestionsMode suggestionsMode = SuggestionsMode.INLINE;
 }
