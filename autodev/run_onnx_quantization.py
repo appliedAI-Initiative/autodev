220853aa970ce63ab24243a495097b5b90f5dbba
Dominik Jain <d.jain@appliedai.de>
2023-07-10 10:34:14 +0200

    Add quantization of ONNX models

---
+++ autodev/run_onnx_quantization.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from autodev import logging
+from autodev.autocomplete.onnx_conversion import ONNXConversion
+
+log = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    logging.configure()
+    log.info("Starting")
+
+    # apply quantization to previously created ONNX model (see run_onnx_conversion)
+    model_name = "santacoder_onnx"
+    input_model_path = Path(f"models/{model_name}")
+    #ONNXConversion.quantize_avx512(input_model_path, Path(f"models/{model_name}_avx512"))
+    ONNXConversion.quantize_avx512_vnni(input_model_path, Path(f"models/{model_name}_avx512-vnni"))
+
+    log.info("Done")
--- autodev/src/autodev/autocomplete/onnx_conversion.py
+++ autodev/src/autodev/autocomplete/onnx_conversion.py
@@ -2,6 +2,8 @@ import logging
 from pathlib import Path
 
 import transformers
+from optimum.onnxruntime import ORTQuantizer
+from optimum.onnxruntime.configuration import AutoQuantizationConfig
 from transformers import PreTrainedModel
 from transformers.onnx import FeaturesManager
 
@@ -36,3 +38,35 @@ class ONNXConversion:
         )
 
         model.config.save_pretrained(output_dir)
+
+    @staticmethod
+    def _quantize(onnx_src_dir: Path, onnx_target_dir: Path, qconfig):
+        """
+        :param onnx_src_dir: source directory containing one or more .onnx models to be quantized
+        :param onnx_target_dir: target directory in which to save the quantized models to be generated
+        :param qconfig: the quantization configuration
+        """
+        for onnx_file_path in onnx_src_dir.glob("*.onnx"):
+            log.info(f"Quantizing {onnx_file_path}")
+            quantizer = ORTQuantizer.from_pretrained(onnx_src_dir, file_name=onnx_file_path.name)
+            quantizer.quantize(qconfig, save_dir=onnx_target_dir)
+
+    @classmethod
+    def quantize_avx512(cls, onnx_src_dir: Path, onnx_target_dir: Path):
+        """
+        :param onnx_src_dir: source directory containing one or more .onnx models to be quantized
+        :param onnx_target_dir: target directory in which to save the quantized models to be generated
+        """
+        # NOTE: This is analogous to the CLI command:
+        # optimum-cli onnxruntime quantize --avx512 --onnx_model src_dir -o target_dir
+        cls._quantize(onnx_src_dir, onnx_target_dir, AutoQuantizationConfig.avx512(is_static=False,
+            per_channel=False))
+
+    @classmethod
+    def quantize_avx512_vnni(cls, onnx_src_dir: Path, onnx_target_dir: Path):
+        """
+        :param onnx_src_dir: source directory containing one or more .onnx models to be quantized
+        :param onnx_target_dir: target directory in which to save the quantized models to be generated
+        """
+        cls._quantize(onnx_src_dir, onnx_target_dir, AutoQuantizationConfig.avx512_vnni(is_static=False,
+            per_channel=False))
