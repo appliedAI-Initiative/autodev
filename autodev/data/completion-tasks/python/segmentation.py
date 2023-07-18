39d003dabb87ffff05273693dfae7f1e59dfe516
Dominik Jain <d.jain@appliedai.de>
2023-07-18 17:13:04 +0200

    Add Python completion tasks

---
+++ autodev/data/completion-tasks/python/segmentation.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def segmentation(seq: List[int], separator: int) -> List[List[int]]:
+    result = []
+    curr = []
+    for i, item in enumerate(seq):
+        if item == <todo>
+    return result
+
+
+if __name__ == '__main__':
+    segmentation([1,2,0,6,2,4,0,6,9,0], 0)
